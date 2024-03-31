d1fee3acc712521923f4782f302f460b666cae36
Karim Ouazzou <karimouazzou@gmail.com>
2024-03-31 16:24:57 +0200

    ui sign in

--- app/src/main/java/tekup/android/foodup/SignInActivity.java
+++ app/src/main/java/tekup/android/foodup/SignInActivity.java
@@ -3,12 +3,35 @@ package tekup.android.foodup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SignInActivity extends AppCompatActivity {
+    private ImageButton imageButtonShowPassword;
+    private EditText editTextPassword;
+    private boolean isPasswordVisible = false;
+    private int cursorPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        imageButtonShowPassword = (ImageButton) findViewById(R.id.imageButtonShowPassword);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        imageButtonShowPassword.setOnClickListener(v -> {
+            isPasswordVisible = !isPasswordVisible;
+            cursorPosition = editTextPassword.getSelectionStart();
+            if (isPasswordVisible) {
+                editTextPassword.setTransformationMethod(null);
+                imageButtonShowPassword.setImageResource(R.drawable.outline_visibility_off_24);
+            } else {
+                editTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                imageButtonShowPassword.setImageResource(R.drawable.outline_visibility_24);
+            }
+            editTextPassword.setSelection(cursorPosition);
+        });
     }
 }
