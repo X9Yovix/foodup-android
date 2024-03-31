50332e1e5479ddbeab9178cad16dbf25d3e58031
Karim Ouazzou <karimouazzou@gmail.com>
2024-03-31 14:27:32 +0200

    splash screen

--- app/src/main/java/tekup/android/foodup/MainActivity.java
+++ app/src/main/java/tekup/android/foodup/MainActivity.java
@@ -1,13 +1,23 @@
 package tekup.android.foodup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
